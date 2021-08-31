f14f8de010a5054f49b99c1f9a9cf506571b6d9c
Nyxordinal <kujyokazunejpn@gmail.com>
2021-08-31 17:55:33 +0700

    Login and home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,119 @@
-import Layout from "@components/layout";
-import { Typography } from "@material-ui/core";
+import useAuth, { ProtectRoute } from '@auth';
+import Layout from '@components/layout';
+import Loader from '@components/loader';
+import { Summary } from '@interface/entity.interface';
+import { CircularProgress, makeStyles, Paper, Typography } from '@material-ui/core';
+import { getExpenseSummary } from '@services/expense.service';
+import { getIncomeSummary } from '@services/income.service';
+import { formatDateShort } from '@util';
+import MaterialTable, { Column } from 'material-table';
+import { Fragment, useEffect, useState } from 'react';
+
+const useStyles = makeStyles({
+  box: {
+    marginBottom: '20px'
+  }
+})
+
+const columnExpense: Column<Summary>[] = [
+  {
+    title: 'Year Month',
+    field: 'yearmonth',
+    type: 'date',
+    render: rowData => formatDateShort(rowData.yearmonth)
+  },
+  {
+    title: 'Price',
+    field: 'amount',
+    type: 'numeric',
+  },
+]
+
+const columnIncome: Column<Summary>[] = [
+  {
+    title: 'Year Month',
+    field: 'yearmonth',
+    type: 'date',
+    render: rowData => formatDateShort(rowData.yearmonth)
+  },
+  {
+    title: 'Amount',
+    field: 'amount',
+    type: 'numeric',
+  },
+]
 
 const IndexPage = () => {
-  return (
+  const { isAuthenticated } = useAuth()
+  const classes = useStyles()
+  const [expenseSummary, setExpenseSummary] = useState<Summary[]>([])
+  const [incomeSummary, setIncomeSummary] = useState<Summary[]>([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const expensesData = await getExpenseSummary()
+      const incomesData = await getIncomeSummary()
+      setExpenseSummary(expensesData)
+      setIncomeSummary(incomesData)
+      setLoading(false)
+    }
+    if (isAuthenticated) fetchData()
+  }, [isAuthenticated]);
+
+  return !isAuthenticated ? (
+    <Loader />
+  ) : (
     <Layout>
-      <Typography>
-        Hello World
-      </Typography>
-    </Layout >
-  )
+      <Fragment>
+        <Typography variant="h4" gutterBottom>Summary Page</Typography>
+        <div className={classes.box}>
+          <Typography variant="h5" gutterBottom>Expense Summary</Typography>
+          {loading ? (
+            <CircularProgress />
+          ) : (
+            <MaterialTable
+              title=""
+              columns={columnExpense}
+              components={{
+                Container: (props) => (
+                  <Paper {...props} elevation={0} />
+                )
+              }}
+              data={expenseSummary}
+              options={{
+                search: true,
+                actionsColumnIndex: -1,
+                addRowPosition: 'first',
+              }}
+            />
+          )}
+        </div>
+        <div className={classes.box}>
+          <Typography variant="h5" gutterBottom>Income Summary</Typography>
+          {loading ? (
+            <CircularProgress />
+          ) : (
+            <MaterialTable
+              title=""
+              columns={columnIncome}
+              components={{
+                Container: (props) => (
+                  <Paper {...props} elevation={0} />
+                )
+              }}
+              data={incomeSummary}
+              options={{
+                search: true,
+                actionsColumnIndex: -1,
+                addRowPosition: 'first',
+              }}
+            />
+          )}
+        </div>
+      </Fragment>
+    </Layout>
+  );
 }
 
-export default IndexPage
+export default ProtectRoute(IndexPage)
---
+++ src/pages/login.tsx
@@ -0,0 +1,139 @@
+import useAuth, { ProtectRoute } from "@auth";
+import { Button, IconButton, InputAdornment, makeStyles, Snackbar, TextField, Typography } from "@material-ui/core";
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import { Alert, Color } from "@material-ui/lab";
+import { ChangeEvent, MouseEvent, useState } from "react";
+
+const useStyles = makeStyles({
+    base: {
+        height: "100vh",
+        background: '#e0e0e0',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    form: {
+        maxWidth: '330px',
+        margin: '0 auto',
+        background: 'white',
+        padding: '20px',
+        borderRadius: '10px',
+    },
+    formButton: {
+        marginTop: '5px'
+    }
+})
+
+const LoginPage = () => {
+    const { login } = useAuth()
+    const classes = useStyles()
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const [open, setOpen] = useState<boolean>(false);
+    const [msg, setMessage] = useState<string>('');
+    const [severity, setSeverity] = useState<Color>('success');
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+
+    const handleChangeEmail = () => (
+        event: ChangeEvent<HTMLInputElement>
+    ) => setEmail(event.target.value)
+    const handleChangePassword = () => (
+        event: ChangeEvent<HTMLInputElement>
+    ) => setPassword(event.target.value)
+    const handleSubmit = async () => {
+        if (email === '' || password === '') {
+            openSnackbar('warning', 'email and password is required')
+        } else {
+            const result = await login(
+                email.trim(),
+                password.trim()
+            )
+            if (!result.success) {
+                openSnackbar('error', result.message as string)
+            }
+        }
+    }
+    const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
+    const handleClickShowPassword = () =>
+        setShowPassword(!showPassword)
+    const handleMouseDownPassword = (event: MouseEvent) =>
+        event.preventDefault()
+    const openSnackbar = (type: Color, message: string) => {
+        setMessage(message)
+        setSeverity(type)
+        setOpen(true)
+    }
+
+    return (
+        <div className={classes.base}>
+            <form className={classes.form} noValidate autoComplete="off">
+                <Typography variant="h4" align="center" gutterBottom>Plutus</Typography>
+                <TextField
+                    id="email"
+                    label="Email"
+                    value={email}
+                    fullWidth
+                    onChange={handleChangeEmail()}
+                    variant='outlined'
+                    required
+                />
+                <TextField
+                    id="password"
+                    label="Password"
+                    value={password}
+                    fullWidth
+                    onChange={handleChangePassword()}
+                    variant='outlined'
+                    margin='normal'
+                    required
+                    type={
+                        showPassword
+                            ? 'text'
+                            : 'password'
+                    }
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {showPassword ? (
+                                        <Visibility />
+                                    ) : (
+                                        <VisibilityOff />
+                                    )}
+                                </IconButton>
+                            </InputAdornment>
+                        )
+                    }}
+                />
+                <Button
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    onClick={handleSubmit}
+                    fullWidth
+                    className={classes.formButton}
+                >
+                    Login
+                </Button>
+            </form>
+            <Snackbar open={open} autoHideDuration={3000} onClose={handleClose} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+                <Alert onClose={handleClose} severity={severity}>
+                    {msg}
+                </Alert>
+            </Snackbar>
+        </div>
+    )
+}
+export default ProtectRoute(LoginPage, true)
