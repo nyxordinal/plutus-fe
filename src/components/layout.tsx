ce3b0a9252b0c20d988bebe8d176e8e1c00220ca
Nyxordinal <kujyokazunejpn@gmail.com>
2021-08-31 17:54:07 +0700

    Components

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,7 @@
+import useAuth from "@auth";
 import {
     AppBar,
-    Collapse,
-    Container,
+    Button, Container,
     CssBaseline,
     Drawer,
     List,
@@ -13,15 +13,10 @@ import {
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet';
-import AddIcon from '@material-ui/icons/Add';
 import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
 import HomeIcon from '@material-ui/icons/Home';
-import ListIcon from '@material-ui/icons/List';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
-import { useState } from "react";
 
 type PropType = {
     children: JSX.Element
@@ -32,7 +27,6 @@ const drawerWidth = 240;
 const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
-        border: '1px solid black'
     },
     appBar: {
         zIndex: theme.zIndex.drawer + 1,
@@ -40,6 +34,9 @@ const useStyles = makeStyles((theme) => ({
     drawer: {
         width: drawerWidth,
         flexShrink: 0,
+    },
+    drawerTitle: {
+        flexGrow: 1,
     },
     drawerPaper: {
         width: drawerWidth,
@@ -58,16 +55,8 @@ const useStyles = makeStyles((theme) => ({
 
 
 const Layout = (props: PropType) => {
+    const { logout } = useAuth()
     const classes = useStyles();
-    const [openExpense, setOpenExpense] = useState(false);
-    const [openIncome, setOpenIncome] = useState(false);
-
-    const handleClickExpense = () => {
-        setOpenExpense(!openExpense);
-    };
-    const handleClickIncome = () => {
-        setOpenIncome(!openIncome);
-    };
 
     return (
         <Container maxWidth="xl" disableGutters={true}>
@@ -75,9 +64,10 @@ const Layout = (props: PropType) => {
                 <CssBaseline />
                 <AppBar position="fixed" className={classes.appBar}>
                     <Toolbar>
-                        <Typography variant="h6" noWrap>
+                        <Typography variant="h6" noWrap className={classes.drawerTitle}>
                             Plutus
                         </Typography>
+                        <Button color="inherit" onClick={logout}>Logout</Button>
                     </Toolbar>
                 </AppBar>
                 <Drawer
@@ -91,65 +81,23 @@ const Layout = (props: PropType) => {
                     <div className={classes.drawerContainer}>
                         <List>
                             <Link href="/" passHref>
-                                <ListItem button key='Home'>
+                                <ListItem button key='home'>
                                     <ListItemIcon><HomeIcon /></ListItemIcon>
                                     <ListItemText primary='Home' />
                                 </ListItem>
                             </Link>
-                            <ListItem button onClick={handleClickExpense}>
-                                <ListItemIcon>
-                                    <AttachMoneyIcon />
-                                </ListItemIcon>
-                                <ListItemText primary="Expense" />
-                                {openExpense ? <ExpandLess /> : <ExpandMore />}
-                            </ListItem>
-                            <Collapse in={openExpense} timeout="auto" unmountOnExit>
-                                <List component="div" disablePadding>
-                                    <Link href="expense" passHref>
-                                        <ListItem button className={classes.nested}>
-                                            <ListItemIcon>
-                                                <ListIcon />
-                                            </ListItemIcon>
-                                            <ListItemText primary="All Expense" />
-                                        </ListItem>
-                                    </Link>
-                                    <Link href="/expense/create" passHref>
-                                        <ListItem button className={classes.nested}>
-                                            <ListItemIcon>
-                                                <AddIcon />
-                                            </ListItemIcon>
-                                            <ListItemText primary="Create Expense" />
-                                        </ListItem>
-                                    </Link>
-                                </List>
-                            </Collapse>
-                            <ListItem button onClick={handleClickIncome}>
-                                <ListItemIcon>
-                                    <AccountBalanceWalletIcon />
-                                </ListItemIcon>
-                                <ListItemText primary="Income" />
-                                {openIncome ? <ExpandLess /> : <ExpandMore />}
-                            </ListItem>
-                            <Collapse in={openIncome} timeout="auto" unmountOnExit>
-                                <List component="div" disablePadding>
-                                    <Link href="/income" passHref>
-                                        <ListItem button className={classes.nested}>
-                                            <ListItemIcon>
-                                                <ListIcon />
-                                            </ListItemIcon>
-                                            <ListItemText primary="All Income" />
-                                        </ListItem>
-                                    </Link>
-                                    <Link href="/income/create" passHref>
-                                        <ListItem button className={classes.nested}>
-                                            <ListItemIcon>
-                                                <AddIcon />
-                                            </ListItemIcon>
-                                            <ListItemText primary="Create Income" />
-                                        </ListItem>
-                                    </Link>
-                                </List>
-                            </Collapse>
+                            <Link href="/expense" passHref>
+                                <ListItem button key='expense'>
+                                    <ListItemIcon><AttachMoneyIcon /></ListItemIcon>
+                                    <ListItemText primary="Expense" />
+                                </ListItem>
+                            </Link>
+                            <Link href="/income" passHref>
+                                <ListItem button key='income'>
+                                    <ListItemIcon><AccountBalanceWalletIcon /></ListItemIcon>
+                                    <ListItemText primary="Income" />
+                                </ListItem>
+                            </Link>
                         </List>
                     </div>
                 </Drawer>
