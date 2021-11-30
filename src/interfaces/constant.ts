2320187b3e497c3a2a0c0bbaa337b5d35a5db938
Nyxordinal <kujyokazunejpn@gmail.com>
2021-12-01 00:51:33 +0700

    chance default date in expense search to current date

--- src/interfaces/constant.ts
+++ src/interfaces/constant.ts
@@ -5,10 +5,6 @@ export const HTTP_STATUS_CREATED = 201
 
 export const SEARCH_TGL_BUTTON_ON_TEXT = 'Turn On Search'
 export const SEARCH_TGL_BUTTON_OFF_TEXT = 'Turn Off Search'
-export const DEFAULT_EXPENSE_SEARCH_VALUES = {
-    name: '',
-    startDate: new Date('2017-01-01'),
-    endDate: new Date('2030-12-31')
-}
+export const DEFAULT_EXPENSE_SEARCH_VALUES_NAME = ''
 
 export const TABLE_ROW_PER_PAGE_OPTION = [5, 10, 25]
