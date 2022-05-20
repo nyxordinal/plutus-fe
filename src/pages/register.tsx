2f856c14be9d61d4b30adbf3e3dca1eab2c9f662
Nyxordinal <nyxordinal@gmail.com>
2022-05-20 14:13:20 +0700

    Reset password

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,9 +1,11 @@
-import useAuth, { ProtectRoute } from "@auth";
+import useAuth from "@auth";
 import { Button, CircularProgress, Divider, IconButton, InputAdornment, makeStyles, Snackbar, TextField, Typography } from "@material-ui/core";
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import { Alert, Color } from "@material-ui/lab";
+import { ValidateEmail } from "@util";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { ChangeEvent, MouseEvent, useState } from "react";
 
 const useStyles = makeStyles({
@@ -34,36 +36,58 @@ const useStyles = makeStyles({
     }
 })
 
-const LoginPage = () => {
-    const { login } = useAuth()
+const RegisterPage = () => {
+    const { register } = useAuth()
     const classes = useStyles()
+    const router = useRouter()
+    const [name, setName] = useState<string>('')
     const [email, setEmail] = useState<string>('')
     const [password, setPassword] = useState<string>('')
+    const [passwordConfirm, setPasswordConfirm] = useState<string>('')
     const [open, setOpen] = useState<boolean>(false);
     const [msg, setMessage] = useState<string>('');
     const [severity, setSeverity] = useState<Color>('success');
     const [showPassword, setShowPassword] = useState<boolean>(false);
     const [loadingLogin, setLoadingLogin] = useState<boolean>(false);
 
+    const handleChangeName = () => (
+        event: ChangeEvent<HTMLInputElement>
+    ) => setName(event.target.value)
     const handleChangeEmail = () => (
         event: ChangeEvent<HTMLInputElement>
     ) => setEmail(event.target.value)
     const handleChangePassword = () => (
         event: ChangeEvent<HTMLInputElement>
     ) => setPassword(event.target.value)
+    const handleChangePasswordConfirm = () => (
+        event: ChangeEvent<HTMLInputElement>
+    ) => setPasswordConfirm(event.target.value)
     const handleSubmit = async () => {
-        if (email === '' || password === '') {
-            openSnackbar('warning', 'Email and password is required')
+        if (name === '') {
+            openSnackbar('warning', 'Name is required')
+        } else if (email === '') {
+            openSnackbar('warning', 'Email is required')
+        } else if (!ValidateEmail(email)) {
+            openSnackbar('warning', 'Please enter a valid email')
+        } else if (password === '' || passwordConfirm === '') {
+            openSnackbar('warning', 'Password and password confirmation is required')
+        } else if (password !== passwordConfirm) {
+            openSnackbar('warning', 'Password and password confirmation are not the same')
+        } else if (password.length < 8) {
+            openSnackbar('warning', 'Minimum password length is 8 characters')
         } else {
             setLoadingLogin(true)
-            const result = await login(
-                email.trim(),
-                password.trim()
-            )
-            !result.success
-                ? openSnackbar('error', result.message)
-                : openSnackbar('success', 'Login success')
-            setLoadingLogin(false)
+            const result = await register(name.trim(), email.trim(), password.trim())
+            if (result.success) {
+                openSnackbar('success', `Registration is successful, you will be redirected to login page`)
+                setLoadingLogin(false)
+                setTimeout(() => {
+                    router.push('/login')
+                }, 5000);
+            } else {
+                openSnackbar('error', result.message)
+                setLoadingLogin(false)
+            }
         }
     }
     const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
@@ -86,7 +110,16 @@ const LoginPage = () => {
     return (
         <div className={classes.base}>
             <form className={classes.form} noValidate autoComplete="off">
-                <Typography variant="h4" align="center" gutterBottom>Plutus</Typography>
+                <Typography variant="h4" align="center" gutterBottom>Plutus | Register</Typography>
+                <TextField
+                    id="name"
+                    label="Name"
+                    value={name}
+                    fullWidth
+                    onChange={handleChangeName()}
+                    variant='outlined'
+                    required
+                />
                 <TextField
                     id="email"
                     label="Email"
@@ -94,6 +127,7 @@ const LoginPage = () => {
                     fullWidth
                     onChange={handleChangeEmail()}
                     variant='outlined'
+                    margin='normal'
                     required
                 />
                 <TextField
@@ -129,7 +163,39 @@ const LoginPage = () => {
                         )
                     }}
                 />
-                <Typography variant="subtitle1" align="left" gutterBottom><Link href={"/password/forgot"}>Forgot password?</Link></Typography>
+                <TextField
+                    id="password-confirmation"
+                    label="Password Confirmation"
+                    value={passwordConfirm}
+                    fullWidth
+                    onChange={handleChangePasswordConfirm()}
+                    variant='outlined'
+                    margin='normal'
+                    required
+                    type={
+                        showPassword
+                            ? 'text'
+                            : 'password'
+                    }
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {showPassword ? (
+                                        <Visibility />
+                                    ) : (
+                                        <VisibilityOff />
+                                    )}
+                                </IconButton>
+                            </InputAdornment>
+                        )
+                    }}
+                />
                 {loadingLogin
                     ? <CircularProgress className={classes.circularProgress} />
                     : <Button
@@ -140,11 +206,11 @@ const LoginPage = () => {
                         fullWidth
                         className={classes.formButton}
                     >
-                        Login
+                        Register
                     </Button>
                 }
                 <Divider className={classes.divider} />
-                <Typography variant="subtitle1" align="center">New to Plutus? <Link href={"/register"}>Create an account</Link></Typography>
+                <Typography variant="subtitle1" align="center">Already have an account? <Link href={"/login"}>Login</Link></Typography>
             </form>
             <Snackbar open={open} autoHideDuration={3000} onClose={handleClose} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
                 <Alert onClose={handleClose} severity={severity}>
@@ -154,4 +220,4 @@ const LoginPage = () => {
         </div>
     )
 }
-export default ProtectRoute(LoginPage, true)
+export default RegisterPage
---
+++ src/services/password.service.ts
@@ -0,0 +1,41 @@
+import { API, AuthAPI } from "@api"
+import { APIResponse, ServiceResponse } from "@interface/http.interface"
+import { getUTCTimestamp, logErrorResponse } from "@util"
+import { AxiosResponse } from "axios"
+import { encryptPayload } from "encryptor"
+
+export const sendForgotPasswordEmail = async (
+    email: string,
+): Promise<ServiceResponse> => {
+    try {
+        const r: APIResponse<null> = await API.post('/auth/forgot', {
+            email,
+        })
+        return { success: true, message: r.message }
+    } catch (error) {
+        return { success: false, message: logErrorResponse(error) }
+    }
+}
+
+export const resetPassword = async (
+    email: string,
+    resetToken: string,
+    password: string
+): Promise<ServiceResponse> => {
+    try {
+        // get server timestamp
+        const resp: AxiosResponse<APIResponse<number>> = await AuthAPI.get('/login-state')
+
+        // encrypt payload
+        const encPassword = encryptPayload(password, resp.data.data || getUTCTimestamp())
+
+        const r: APIResponse<null> = await API.post('/auth/reset', {
+            email,
+            token: resetToken,
+            enc_password: encPassword
+        })
+        return { success: true, message: r.message }
+    } catch (error) {
+        return { success: false, message: logErrorResponse(error) }
+    }
+}
