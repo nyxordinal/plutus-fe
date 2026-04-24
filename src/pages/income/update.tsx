5f223634e20e923d84bd3293951f4585b7c00287
Nyxordinal <nyxordinal@gmail.com>
2026-04-24 18:26:04 +0700

    trigger submit when clicking enter button on update screens

--- src/pages/income/update.tsx
+++ src/pages/income/update.tsx
@@ -111,7 +111,7 @@ const Update = () => {
           customUrl={"/income/update"}
         />
         <div className="relative bg-blueGray-800 md:pt-32 pb-32 pt-12">
-          <div className="px-4 md:px-10 mx-auto w-full">
+          <form onSubmit={(e) => { e.preventDefault(); handleSubmit(); }} className="px-4 md:px-10 mx-auto w-full">
             <div className="lg:w-6/12 xl:w-3/12 px-4 mb-3">
               <h6 className="text-xl font-normal leading-normal mt-0 mb-2 text-white">
                 {translate("source")}
@@ -158,11 +158,7 @@ const Update = () => {
             <div className="lg:w-6/12 xl:w-3/12 px-4">
               <button
                 className="bg-white text-blueGray-800 active:bg-blueGray-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={(e) => {
-                  e.preventDefault();
-                  handleSubmit();
-                }}
+                type="submit"
               >
                 {isLoading ? (
                   <CircularProgress size={20} />
@@ -171,7 +167,7 @@ const Update = () => {
                 )}
               </button>
             </div>
-          </div>
+          </form>
         </div>
         <div className="px-4 md:px-10 mx-auto w-full -m-24">
           <div className="flex flex-wrap mt-4">
